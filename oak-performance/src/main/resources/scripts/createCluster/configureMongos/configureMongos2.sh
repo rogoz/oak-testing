baf3d16ead026ccfcc56ff037c1af1d721819056
Tudor Rogoz <rogoz@adobe.com>
2013-06-16 12:31:31 +0300

    Create a mongo database cluster in amazon using provisionr and rundeck

---
+++ oak-performance/src/main/resources/scripts/createCluster/configureMongos/configureMongos2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+ 
+# Configure the mongos stations 2.Enable sharding on DATABASE_NAME, and sets the sharding key for blobs and nodes collections.
+ 
+MONGOS_MAIN=`xmllint --xpath 'string(/project/node[1]/@hostname)' mongos.xml` 
+CURRENT_NODE=@node.hostname@ 
+MONGOS_PORT=27017 
+MONGOD_PORT=27017 
+DATABASE_NAME=Oak 
+# get shards Ips 
+ 
+TEMP=`xmllint --xpath '/project/node/@hostname' shards.xml|sed -e "s/ hostname=/ /g"| sed -e "s/\"/'/g"` 
+declare -a shards=($TEMP) 
+echo shards=${shards[@]} 
+ 
+ 
+# configure the cluster from the main platform 
+if [ "$CURRENT_NODE" == "$MONGOS_MAIN" ]; then 
+	echo MONGOS $MONGOS_MAIN starting to configure the shards 
+	for shard in "${shards[@]}" 
+	do 
+	  shard_trim=`echo $shard|tr -d ''\'''` 
+	  echo "Link shard:sh.addShard(\"${shard_trim}:${MONGOD_PORT}\")" 
+	  mongo --host $MONGOS_MAIN admin --port $MONGOS_PORT --eval "sh.addShard(\"${shard_trim}:${MONGOD_PORT}\")" 
+	  sleep 2 
+	done 
+	mongo --host $MONGOS_MAIN $DATABASE_NAME --port $MONGOS_PORT --eval "sh.enableSharding(\"$DATABASE_NAME\")" 
+	# Set sharding key for nodes and blobs 
+	mongo --host $MONGOS_MAIN $DATABASE_NAME --port $MONGOS_PORT --eval "sh.shardCollection(\"$DATABASE_NAME.nodes\", { \"_id\": 1 }, true)" 
+	mongo --host $MONGOS_MAIN $DATABASE_NAME --port $MONGOS_PORT --eval "sh.shardCollection(\"$DATABASE_NAME.blobs\", { \"_id\": 1 }, true)" 
+fi
---
+++ oak-performance/src/main/resources/scripts/createCluster/createShards/createShards.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+ 
+# Create a database shard node and downloads the shards connection parameters on the local server.This script should run on the rundeck local server.
+# SHARDS_NUMBER and HDD_SIZE are defined as rundeck options.  
+
+PROVISIONR_HOST=localhost 
+PROVISIONR_PORT=8181 
+PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/ 
+RAND=$(( $RANDOM % 100000 ))
+SHARDS_KEY=s${RAND} 
+${PROVISIONR_PATH}client "provisionr:create --id amazon --key ${SHARDS_KEY} --size @option.SHARDS_NUMBER@ --volume /dev/sdh1:@option.HDD_SIZE@ --volume /dev/sdh2:@option.HDD_SIZE@ --volume /dev/sdh3:@option.HDD_SIZE@ --volume /dev/sdh4:@option.HDD_SIZE@ --hardware-type m1.large --template mongod --image-id ami-4965f479 --timeout 1200" 
+ 
+# wait 5 minutes for the instances to be created 
+sleep 300 
+ 
+# get the shards machine.xml file 
+ 
+rm -rf shards.xml 
+wget http://${PROVISIONR_HOST}:${PROVISIONR_PORT}/rundeck/machines.xml -O shards.xml
