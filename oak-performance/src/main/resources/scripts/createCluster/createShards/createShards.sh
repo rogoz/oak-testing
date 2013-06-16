0047aa255a36064706a4dd59c5240a6a58086353
Tudor Rogoz <rogoz@adobe.com>
2013-06-16 14:18:07 +0300

    Test options

--- oak-performance/src/main/resources/scripts/createCluster/createShards/createShards.sh
+++ oak-performance/src/main/resources/scripts/createCluster/createShards/createShards.sh
@@ -8,8 +8,8 @@ PROVISIONR_PORT=8181
 PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/ 
 RAND=$(( $RANDOM % 100000 ))
 SHARDS_KEY=s${RAND} 
-SHARDS_NUMBER=@option.SHARDS_NUMBER@
-HDD_SIZE=@option.HDD_SIZE@
+SHARDS_NUMBER=$1
+HDD_SIZE=$2
 ${PROVISIONR_PATH}client "provisionr:create --id amazon --key ${SHARDS_KEY} --size ${SHARDS_NUMBER} --volume /dev/sdh1:${HDD_SIZE} --volume /dev/sdh2:${HDD_SIZE} --volume /dev/sdh3:${HDD_SIZE} --volume /dev/sdh4:${HDD_SIZE} --hardware-type m1.large --template mongod --image-id ami-4965f479 --timeout 1200" 
  
 # wait 5 minutes for the instances to be created 
