569672ed26561ae1bcf04dd2e0091f1b80041d96
Tudor Rogoz <rogoz@adobe.com>
2013-07-09 16:35:00 +0300

    Remove nodes script added

--- oak-performance/src/main/resources/scripts/createCluster/removeNodes/removeAllNodes.sh
+++ oak-performance/src/main/resources/scripts/createCluster/removeNodes/removeAllNodes.sh
@@ -6,6 +6,6 @@
 RAND=$(( $RANDOM % 1000 ))
 # KEY=k${RAND}
 PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/
-KEY=s6065
+KEY=$1
 ${PROVISIONR_PATH}client "provisionr:destroy -k ${KEY}"
 # wait for the instances to be created
