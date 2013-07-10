1faffae79d73e339a0aa4adb9b730729cdebef1c
Tudor Rogoz <rogoz@adobe.com>
2013-07-10 12:15:45 +0300

    Add 2 steps for starting the tests and collectiong the results

--- oak-performance/src/main/resources/scripts/createCluster/runWorkflow.sh
+++ oak-performance/src/main/resources/scripts/createCluster/runWorkflow.sh
@@ -6,7 +6,7 @@ SHARDS_NUMBER=3
 MONGOS_NUMBER=3
 HDD_SIZE=25
 PROVISIONR_PATH=/home/tudor/repos/incubator-provisionr/karaf/assembly/target/org.apache.provisionr-0.4.0-incubating-SNAPSHOT/bin/
-
+TEST_NAME="OAKTest#testFlatStructure"
 
 # Create shards
 echo "***** Step 1: Creating cluster's shards. *****"
@@ -29,3 +29,14 @@ echo "***** Step 3: Completed. *****"
 echo "***** Step 4: Configure mongos platforms. *****"
 run -i 6 --follow
 echo "***** Step 4: Completed. *****"
+
+#Start test
+echo "***** Step 5: Start tests. *****"
+run -i 9 --follow -- -TEST_NAME ${TEST_NAME}
+echo "***** Step 5: Completed. *****"
+
+
+#Collect results
+echo "***** Step 6: Collect results. *****"
+run -i 10 --follow
+echo "***** Step 6: Completed. *****"
