78693780ce9ff9c89a89cc39c0ca51c4adfd5efc
Tudor Rogoz <rogoz@adobe.com>
2013-08-07 12:08:16 +0300

    Add start oak tests job

--- oak-performance/src/main/resources/scripts/startTests/startOakTests.sh
+++ oak-performance/src/main/resources/scripts/startTests/startOakTests.sh
@@ -19,9 +19,10 @@ OAK_TYPE=$3
 DATABASE_NAME=Oak     
 CLUSTER_NODE_ID=${CURRENT_NODE:0:15}     
 PROCESS_TIMEOUT=30     
-TEMP=`xmllint --xpath '/project/node/@hostname' shards.xml|sed -e "s/ hostname=/ /g"| sed -e "s/\"/'/g"` 
-declare -a shards=($TEMP)
-MONGOS_NUMBER=${#shards[@]}
+TEMP=`xmllint --xpath '/project/node/@hostname' mongos.xml|sed -e "s/ hostname=/ /g"| sed -e "s/\"/'/g"` 
+declare -a mongos=($TEMP)
+MONGOS_NUMBER=${#mongos[@]}
+echo "MONGOS_NUMBER="${MONGOS_NUMBER}
 
 # Download and set Java  
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk6-downloads-1637591.html;" http://download.oracle.com/otn-pub/java/jdk/6u33-b03/jdk-6u33-linux-x64.bin   
@@ -56,7 +57,6 @@ if [ "$CURRENT_NODE" == "$MONGOS_MAIN_PLATFORM" ]; then
     mongo --host localhost $DATABASE_NAME --port $MONGOS_PORT --eval "db.results.remove()"  
     mongo --host localhost $DATABASE_NAME --port $MONGOS_PORT --eval "db.summary.remove()"     
     mongo --host localhost $DATABASE_NAME --port $MONGOS_PORT --eval "db.journals.remove()"      
-
     # start the test on the main mongos platform (workaround for the concurrent oak repositories init)   
     nohup mvn -f /home/${USER}/oak-testing/oak-performance/pom.xml clean test -Dorg.slf4j.simpleLogger.logFile=Oakmk.log -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dtest=${TEST_NAME} -Dmongos.number=${MONGOS_NUMBER} -Dcluster.node=${CLUSTER_NODE_ID} -Doak.type=${OAK_TYPE} &
     PID=`echo $!`  
