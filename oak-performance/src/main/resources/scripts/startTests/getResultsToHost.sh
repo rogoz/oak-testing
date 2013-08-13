e982c140e8f793698c6b5d09ee4b000600cd9738
Tudor Rogoz <rogoz@adobe.com>
2013-08-13 14:56:34 +0200

    Get the results from mongos to rundeck server

---
+++ oak-performance/src/main/resources/scripts/startTests/getResultsToHost.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RESULT_HTML=results.html
+SUMMARY=resultsStats.json
+DBSUMMARY=resultsDBStats.json
+MONGOS_MAIN_PLATFORM=`xmllint --xpath 'string(/project/node[1]/@hostname)' mongos.xml`
+OUTPUT_DIR=$1
+
+mkdir -p $OUTPUT_DIR
+scp ${USER}@${MONGOS_MAIN_PLATFORM}:/home/${USER}/${RESULT_HTML} ${OUTPUT_DIR}/${RESULT_HTML}
